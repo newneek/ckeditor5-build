e8b73b9367246af477e8215743f0d6974af7e167
Seungyeol <seungyeol@publy.co>
2019-01-16 22:41:42 +0900

    change the media-embed plugin code
    Changed the plugin code because the media class name is likely to be duplicated and rebuild

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -22,7 +22,6 @@ import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
@@ -30,6 +29,7 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Font from '@ckeditor/ckeditor5-font/src/font';
 import Quotes from './plugins/ckeditor5-quotes/src/quotes';
 import Comment from './plugins/ckeditor5-comment/src/comment';
+import MediaEmbed from './plugins/ckeditor5-media-embed/src/mediaembed';
 
 export default class InlineEditor extends InlineEditorBase {}
 
