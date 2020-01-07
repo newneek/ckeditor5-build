8aeed1c29b2fb771b2aea4726ea4026b75a694ef
Hungjoon <hungjoon@publy.co>
2020-01-07 19:17:50 +0900

    [WIP]

--- src/plugins/ckeditor5-link-default-schema/src/rexlink.js
+++ src/plugins/ckeditor5-link-default-schema/src/rexlink.js
@@ -24,30 +24,124 @@ export default class LinkDefaultSchema extends Plugin {
     this.linkFormView = linkUI.formView;
     this.linkActionView = linkUI.actionsView;
 
-    this.linkFormView.on('submit', () => {
-      console.log('submit begin');
-      const linkCommand = editor.commands.get('link');
-      const url = linkCommand.value;
-      const withHttp = this._withHttp(url, defaultSchema);
 
-      if (url !== withHttp) {
-        // this.linkFormView.set('value', withHttp);
-        // this.linkActionView.set('href', withHttp);
-        linkCommand.value = withHttp;
-        this.linkFormView.urlInputView.value = withHttp;
-        this.linkActionView.href = withHttp;
-      }
+    // this.linkFormView.on('render', (props) => {
+    //   const currentUrl = this.linkActionView.href;
+    //   console.log('render: ', currentUrl, ' & ', props);
+    //   if (!currentUrl) {
+    //     editor.execute( 'link', `${defaultSchema}://`);
+    //   }
+    // });
+
+
+    editor.on('set', (evt, propertyName, newValue, oldValue) => {
+      console.log('editor.change');
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `Current property value is ${ editor[ propertyName ] }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+      return newValue;
     });
 
-    this.linkFormView.on('change', (eventInfo, name, value, oldValue) => {
-      console.log('linkFormView', eventInfo, name, value, oldValue);
+    editor.on('set:linkHref', (evt, propertyName, newValue, oldValue) => {
+      console.log('editor.linkHref');
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `Current property value is ${ editor[ propertyName ] }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+
+      return this._withHttp(newValue, defaultSchema);
     });
-    this.linkFormView.urlInputView.on('change', (eventInfo, name, value, oldValue) => {
-      console.log('linkFormView.urlInputView', eventInfo, name, value, oldValue);
+
+    editor.on('set:href', (evt, propertyName, newValue, oldValue) => {
+      console.log('editor.href');
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `Current property value is ${ editor[ propertyName ] }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+
+      return this._withHttp(newValue, defaultSchema);
     });
-    this.linkActionView.on('change', (eventInfo, name, value, oldValue) => {
-      console.log('linkActionView', eventInfo, name, value, oldValue);
+
+    linkUI.on('set:href', (evt, propertyName, newValue, oldValue) => {
+      console.log('editor.href');
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `Current property value is ${ editor[ propertyName ] }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+
+      return this._withHttp(newValue, defaultSchema);
     });
+
+    this.linkFormView.urlInputView.on('set:value', (evt, propertyName, newValue, oldValue) => {
+      console.log('this.linkFormView');
+      console.log(evt);
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+
+      return this._withHttp(newValue, defaultSchema);
+    });
+
+    this.linkActionView.on('set:href', (evt, propertyName, newValue, oldValue) => {
+      console.log('this.linkActionView');
+      console.log(evt);
+      console.log( `Value is going to be changed from ${ oldValue } to ${ newValue }` );
+      console.log( `_withHttp value is ${ this._withHttp(newValue, defaultSchema) }` );
+
+      return this._withHttp(newValue, defaultSchema);
+    });
+
+
+    // this.linkFormView.once('render', () => {
+    //   // to use this in for each callback
+    //   const that = this;
+    //
+    //   rexlinkConfig.forEach(function (item, index, array) {
+    //     if (item == 'internal') {
+    //       // Render button's tamplate.
+    //       that.linkButton.render();
+    //       // Register the button under the link form view, it will handle its destruction.
+    //       that.linkFormView.registerChild(that.linkButton);
+    //       // Inject the element into DOM.
+    //       that.linkFormView.element.insertBefore(that.linkButton.element, that.linkFormView.saveButtonView.element);
+    //     }
+    //     if (item == 'media') {
+    //       // Render button's tamplate.
+    //       that.mediaButton.render();
+    //       // Register the button under the link form view, it will handle its destruction.
+    //       that.linkFormView.registerChild(that.mediaButton);
+    //       // Inject the element into DOM.
+    //       that.linkFormView.element.insertBefore(that.mediaButton.element, that.linkFormView.saveButtonView.element);
+    //     }
+    //   });
+    //   console.log('render');
+    //   this.listenTo( that, 'submit', () => {
+    //     console.log(arguments);
+    //     console.log('link');
+    //   });
+    //
+    // });
+
+    // this.linkFormView.on('submit', () => {
+    //   console.log('submit begin');
+    //   const linkCommand = editor.commands.get('link');
+    //   const url = linkCommand.value;
+    //   const withHttp = this._withHttp(url, defaultSchema);
+    //
+    //   if (url !== withHttp) {
+    //     // this.linkFormView.set('value', withHttp);
+    //     // this.linkActionView.set('href', withHttp);
+    //     linkCommand.value = withHttp;
+    //     this.linkFormView.urlInputView.value = withHttp;
+    //     this.linkActionView.href = withHttp;
+    //   }
+    // });
+
+    // this.linkFormView.on('change', (eventInfo, name, value, oldValue) => {
+    //   console.log('linkFormView', eventInfo, name, value, oldValue);
+    // });
+    // this.linkFormView.urlInputView.on('change', (eventInfo, name, value, oldValue) => {
+    //   console.log('linkFormView.urlInputView', eventInfo, name, value, oldValue);
+    // });
+    // this.linkActionView.on('change', (eventInfo, name, value, oldValue) => {
+    //   console.log('linkActionView', eventInfo, name, value, oldValue);
+    // });
 
   }
 
