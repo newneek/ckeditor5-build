1496f2cee72311fab1ef3d5eb54a78643eff749e
Hungjoon <hungjoon@publy.co>
2020-01-09 18:11:17 +0900

    [WIP] Working solution 2

--- src/plugins/ckeditor5-link-default-schema/src/rexlink.js
+++ src/plugins/ckeditor5-link-default-schema/src/rexlink.js
@@ -4,6 +4,8 @@ import LinkUI from '@ckeditor/ckeditor5-link/src/linkui';
 /**
  * @extends module:core/plugin~Plugin
  */
+
+const URLSchemaPattern = /(http(s?)):\/\//gi;
 export default class LinkDefaultSchema extends Plugin {
   /**
    * @inheritDoc
@@ -21,20 +23,127 @@ export default class LinkDefaultSchema extends Plugin {
     const linkCommand = editor.commands.get( 'link' );
     const defaultSchema = editor.config.get('link.defaultSchema');
 
+    const linkUI = editor.plugins.get(LinkUI);
+    this.linkFormView = linkUI.formView;
+    this.linkActionView = linkUI.actionsView;
+
+    // 저장하기 직전에 url 에 default schema 가 적용된다. (schema 가 없는 경우에만)
     linkCommand.on('set:value', (evt, propertyName, newValue, oldValue) => {
-      console.log('this.linkCommand');
+      console.log(evt);
+      console.log('linkCommand (set:value)');
+      console.log(`  ${oldValue} -> ${newValue}`);
 
-      if (!newValue) {
-        console.log( `Value is empty adding default schema (old: ${ oldValue }, new: ${ newValue }), withSchema: ${defaultSchema}://` );
-        evt.return = `${defaultSchema}://`;
+      if (oldValue === undefined && newValue !== undefined) {
+        console.log('  Apply Schema');
+        evt.return = this._withHttp(newValue, defaultSchema);
       }
-
-      console.log( `Value is not going to change (old: ${ oldValue }, new: ${ newValue })` );
     });
 
-    // const linkUI = editor.plugins.get(LinkUI);
-    // this.linkFormView = linkUI.formView;
-    // this.linkActionView = linkUI.actionsView;
+    // 버그 발생 (클릭 할 때마다 action 의 값이 변경된다.)
+    // linkCommand.on('set:value', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkCommand (set:value)');
+    //   console.log(`  ${oldValue} -> ${newValue}`);
+    //
+    //   if (oldValue === undefined && newValue === undefined) {
+    //     console.log('  Apply Schema');
+    //     evt.return = `${defaultSchema}://`;
+    //   }
+    // });
+
+    //
+    // editor.on('set', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('editor - set');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // editor.on('update', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('editor - update');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // editor.on('set:hasAny', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('editor - set:hasAny');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // editor.on('set:link0', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('editor - link0');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // editor.on('set:linkHref', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('editor - linkHref');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // linkUI.on('set', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkUI - set');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // linkUI.on('set:', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkUI - set:');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // linkCommand.on('set', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkCommand - set');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // linkCommand.on('set:value', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkCommand - set:value');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // this.linkFormView.on('set:value', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkFormView - set:value');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // this.linkFormView.urlInputView.on('set:value', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkFormView.urlInputView - set:value');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // this.linkActionView.on('set:href', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('linkActionView - set:href');
+    //   console.log(`  ${propertyName}: ${oldValue} -> ${newValue}`);
+    //   console.log('');
+    // });
+    //
+    // this.linkFormView.on('render', (evt, propertyName, newValue, oldValue) => {
+    //   console.log(evt, propertyName, newValue, oldValue);
+    // })
+
+
+
+
+
+
+
+    // linkCommand.on('set:value', (evt, propertyName, newValue, oldValue) => {
+    //   console.log('this.linkCommand');
+    //
+    //   if (!newValue) {
+    //     console.log( `Value is empty adding default schema (old: ${ oldValue }, new: ${ newValue }), withSchema: ${defaultSchema}://` );
+    //     evt.return = `${defaultSchema}://`;
+    //   }
+    //
+    //   console.log( `Value is not going to change (old: ${ oldValue }, new: ${ newValue })` );
+    // });
+
 
     // this.linkFormView.on('render', (props) => {
     //   const currentUrl = this.linkActionView.href;
@@ -153,10 +262,16 @@ export default class LinkDefaultSchema extends Plugin {
 
   }
 
-
+  _checkURLSchema(url) {
+    return URLSchemaPattern.test(url);
+  }
 
   _withHttp(url, defaultSchema) {
     if (/(http(s?)):\/\//gi.test(url)) {
+      return url;
+    }
+
+    if (!defaultSchema) {
       return url;
     }
 
